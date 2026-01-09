bbcd201be751375fac98f90c45811aca59fad061
FaYeest <mtbfarrasandi@gmail.com>
2026-01-09 14:28:36 +0700

    ci: remove unused import in mock.dart to fix lint error

--- test/mock.dart
+++ test/mock.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 
 void setupFirebaseAuthMocks() {
   TestWidgetsFlutterBinding.ensureInitialized();
